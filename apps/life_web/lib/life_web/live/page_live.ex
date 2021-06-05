3de097fd16ea44a5bd292db5da577528e4924301
Gustavo Chapim <gustavo.chapim@petlove.com.br>
2021-06-05 12:05:25 -0300

    Implement view

--- apps/life_web/lib/life_web/live/page_live.ex
+++ apps/life_web/lib/life_web/live/page_live.ex
@@ -1,39 +1,40 @@
 defmodule LifeWeb.PageLive do
   use LifeWeb, :live_view
 
+  import LifeWeb.Helpers.Grid
+
+  alias Life.Grid
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, assign(socket, grid: set_grid(), grid_size: %{x: x_size(), y: y_size()})}
+  end
+
+  defp set_grid do
+    grid = Grid.new(initial_state())
+
+    send_tick(grid, start_delay())
+    Process.send_after(self(), :reset, reset_tick_multiplier() * tick())
+
+    grid
+  end
+
+  defp send_tick(grid, time \\ tick()) do
+    Process.send_after(self(), {:tick, grid}, time)
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_info({:tick, grid}, socket) do
+    grid = Grid.next_grid(grid)
+    tick_ref = send_tick(grid)
+
+    {:noreply, assign(socket, grid: grid, tick_ref: tick_ref)}
   end
 
   @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
+  def handle_info(:reset, %{assigns: %{tick_ref: tick_ref}} = socket) do
+    Process.cancel_timer(tick_ref)
 
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  defp search(query) do
-    if not LifeWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+    {:noreply, assign(socket, grid: set_grid())}
   end
 end
--- apps/life_web/test/life_web/live/page_live_test.exs
+++ apps/life_web/test/life_web/live/page_live_test.exs
@@ -3,9 +3,38 @@ defmodule LifeWeb.PageLiveTest do
 
   import Phoenix.LiveViewTest
 
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+  test "render", %{conn: conn} do
+    assert {:ok, page_live, _html} = live(conn, "/")
+
+    Enum.each(0..6, fn x_el ->
+      Enum.each(0..6, fn y_el ->
+        assert has_element?(page_live, "#grid-#{x_el}-#{y_el}")
+      end)
+    end)
+
+    assert has_element?(page_live, "#grid-2-1.alive")
+    assert has_element?(page_live, "#grid-2-2.alive")
+    assert has_element?(page_live, "#grid-2-3.alive")
+
+    Process.sleep(20)
+    assert has_element?(page_live, "#grid-1-2.alive")
+    assert has_element?(page_live, "#grid-2-2.alive")
+    assert has_element?(page_live, "#grid-3-2.alive")
+  end
+
+  test "resets state", %{conn: conn} do
+    assert {:ok, page_live, _html} = live(conn, "/")
+
+    Process.sleep(30)
+    assert has_element?(page_live, "#grid-1-2.alive")
+    assert has_element?(page_live, "#grid-2-2.alive")
+    assert has_element?(page_live, "#grid-3-2.alive")
+
+    Process.sleep(20)
+
+    render(page_live)
+    assert has_element?(page_live, "#grid-2-1.alive")
+    assert has_element?(page_live, "#grid-2-2.alive")
+    assert has_element?(page_live, "#grid-2-3.alive")
   end
 end
